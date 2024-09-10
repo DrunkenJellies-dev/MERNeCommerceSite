8cb0092d836572a04715c2ab572df2460f156bc6
CallumNewman <ballumnew@gmail.com>
2024-09-10 22:29:07 +0100

    Added user handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,18 @@ import User from "../models/userModel.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
 
 const createUser = asyncHandler(async (req, res) => {
-  res.send("hello");
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    throw new Error('Please fill all the input fields.')
+  }
+
+  //checking if the user 
+  const userExists = await User.findOne({email});
+
+  if (userExists){
+    res.status(400).send("User already exists");
+  }
 });
 
 export { createUser };
