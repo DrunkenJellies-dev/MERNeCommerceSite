45d86b1287f46f56eac3c10ebcc400d69f44e45d
CallumNewman <ballumnew@gmail.com>
2024-09-14 18:08:12 +0100

    Connected MongoDB and Created a User with connection string

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,14 +5,30 @@ const createUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
 
   if (!username || !email || !password) {
-    throw new Error('Please fill all the input fields.')
+    throw new Error("Please fill all the input fields.");
   }
 
-  //checking if the user 
-  const userExists = await User.findOne({email});
+  //checking if the user
+  const userExists = await User.findOne({ email });
 
-  if (userExists){
-    res.status(400).send("User already exists");
+  if (userExists) {
+    res.status(400).send("User already exists.");
+  }
+
+  const newUser = new User({ username, email, password });
+
+  try {
+    await newUser.save();
+
+    res.status(201).json({
+      _id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+    });
+  } catch (error) {
+    res.status(400);
+    throw new Error("Invalid user data.");
   }
 });
 
