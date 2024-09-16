e3d47588c522b7cfd08df327a2a4fdbcc87699d4
CallumNewman <ballumnew@gmail.com>
2024-09-17 00:04:01 +0100

    Created user authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -65,12 +65,12 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const logoutCurrentUser = asyncHandler(async (req, res) => {
-  res.cookie('jwt', '', {
+  res.cookie("jwt", "", {
     htpOnly: true,
     expires: new Date(0),
-  })
+  });
 
-  res.status(200).json({message: "Logged out successfully!"})
-})
+  res.status(200).json({ message: "Logged out successfully!" });
+});
 
-export { createUser, loginUser, logoutCurrentUser };
+export { createUser, loginUser, logoutCurrentUser, getAllUsers };
